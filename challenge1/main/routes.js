fc43fdd3f81eca8d144d39d9bc707e488020edb2
Carlin Au <carlinau@gmail.com>
2017-09-12 10:15:32 -0400

    Finished both /messages GET and POST endpoints, publishing on heroku

--- challenge1/main/routes.js
+++ challenge1/main/routes.js
@@ -5,15 +5,24 @@ const crypto = require('crypto');
 var table = {};
 
 const init = function RouteHandler(app) {
-  app.get('/message/:hash', (req, res)=>{
-    res.status(200).send(table[req.params.hash]);
+  app.get('/messages/:hash', (req, res)=>{
+    // Check if the digest exists in the table, if it does return its value
+    if (req.params.hash in table) {
+      res.status(200).json({message: table[req.params.hash]});
+    } else {
+      // Return 404 if digest doesn't exist
+      res.status(404).json({err_msg: "Message not found"});
+    }
   });
   app.post('/messages', (req, res)=>{
     const hash = crypto.createHash('sha256');
     hash.update(req.body.message);
     var key = hash.digest('hex');
-    table[key] = req.body.message;
-    res.status(200).send(key);
+    // Check if the hash we found already exists, if it doesn't add it
+    if (!(key in table)) {
+      table[key] = req.body.message;
+    }
+    res.status(200).json({digest: key});
   });
 }
 
