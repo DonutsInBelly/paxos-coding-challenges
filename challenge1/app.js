e33a0d36a6f2b7f5eb992cd6e4c7830f446e1b4e
Carlin Au <carlinau@gmail.com>
2017-09-11 20:43:18 -0400

    Added dotenv to dependencies and .env.example

--- challenge1/app.js
+++ challenge1/app.js
@@ -3,9 +3,12 @@ const helmet  = require('helmet');
 const express = require('express');
 
 // Set up and configure the application
+require('dotenv').config();
 var app = express();
 var port = process.env.PORT || 8080; // Environment variable from heroku, if none, use 8080
 app.use(helmet()); // Use helmet for protection
+
+// Set up Routes
 
 // Launch Application
 app.listen(port);
