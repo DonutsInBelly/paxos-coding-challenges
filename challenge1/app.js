8d0de55f3e1ab54a5c40c50a5414662db114e162
Carlin Au <carlinau@gmail.com>
2017-09-11 21:28:11 -0400

    Removed dotenv as its not necessary

--- challenge1/app.js
+++ challenge1/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const routes  = require('./main/routes.js');
 
 // Set up and configure the application
-require('dotenv').config(); // Load Environment variables from .env file
 var app = express();
 var port = process.env.PORT || 8080; // Environment variable from heroku, if none, use 8080
 app.use(helmet()); // Use helmet for protection
